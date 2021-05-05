c7cc63f925f465c0d46c6587b75fadc8241c527d
Achar <acharsr@nci-02090706-ml.fios-router.home>
2021-05-04 22:06:28 -0400

    Fixed misc location in setup scripts

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="plateypus",
-    version="0.1.10",
+    version="0.1.11",
     author = "Sooraj Achar",
     author_email = "acharsr@nih.gov",
     description = "Processes and plots high throughput cytometry experiments through a GUI",
