5c09855f9a4aa12b82167236eea0543f8cf39e42
Achar <sooraj.achar@gmail.com>
2023-01-04 08:18:48 -0500

    Matplotlib 3.6 breaks plate layout creation; currently requiring older version

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from setuptools import setup,find_packages
     
 install_requires = [
+'matplotlib<=3.5.2',
 'pandas',
 'seaborn',
 'scipy',
@@ -29,7 +30,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="plateypus",
-    version="0.5.4",
+    version="0.5.6",
     author = "Sooraj Achar",
     author_email = "acharsr@nih.gov",
     description = "Processes and plots high throughput cytometry experiments through a GUI",
